eb5e8b5685a5c6287b7d1ad30dc5849ba9da616e
Mfiso1 <you@example.com>
2023-02-17 20:31:59 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The programme uses printf function to output a message
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
