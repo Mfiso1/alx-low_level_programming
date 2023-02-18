448a34a89fea02139b8043e71a0278a56295ded3
Mfiso1 <you@example.com>
2023-02-17 20:24:11 -0800

    task1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program prints a new line using onlu a put function
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
