00329a1fcec7bbc888643302c4dbe577a8888900
Mfiso1 <you@example.com>
2023-02-17 21:15:58 -0800

    hey

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19")
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
 sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 return (1);
 }
