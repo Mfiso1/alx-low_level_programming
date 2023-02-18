1e27a939f5d33d7ce031c1d4c1deaa8aaf9cb820
Mfiso1 <you@example.com>
2023-02-17 21:11:09 -0800

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printf program
+ * Return: 1 (success)
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19")
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
+}
