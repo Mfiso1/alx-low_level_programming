46c20de4174363446f577758af789612ca909eeb
Mfiso1 <you@example.com>
2023-02-20 16:59:53 -0800

    Trailing white space

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,11 +10,10 @@ int main(void)
 {
 	int base;
 
-	for (base = 0; base < 10';base++)
+	for (base = 0; base < 10'; base++)
 	{
-		putchar(base);
+		printf("%d", base);
 	}
-	putchar('\n');
 	return (0);
 }
 
