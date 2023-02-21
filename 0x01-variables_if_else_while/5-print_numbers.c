2df544e78babce5c790dcf0b16f1bb318cb4de40
Mfiso1 <you@example.com>
2023-02-20 16:51:50 -0800

    Be more observant

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -Entry point
+ * Decription: print 0123456789
+ * Return: 0 (sucess)
+ */
+
+int main(void)
+{
+	int base;
+
+	for (base = 0; base < 10';base++)
+	{
+		putchar(base);
+	}
+	putchar('\n');
+	return (0);
+}
+
