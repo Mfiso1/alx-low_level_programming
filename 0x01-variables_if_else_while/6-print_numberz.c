ac8b21730ca54e05b898d9f088c79274c6b2f3c4
Mfiso1 <you@example.com>
2023-02-20 17:14:17 -0800

    Breaking the terminal and me

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -Entry point
+ * Decription: print 0123456789
+ * Return: 0 (sucess)
+ */
+
+int main(void)
+{
+	int base;
+
+	for (base = 0; base < 10; base++)
+	{
+		putchar(base = base + '0');
+	}
+	putchar('\n');
+	return (0);
+}
+
