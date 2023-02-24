ce5952261c76423ee91eef4320ea692f27facea4
Mfiso1 <you@example.com>
2023-02-23 18:13:54 -0800

    Task 4, hi

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,17 @@
 #include "main.h"
 /**
- * print_most_numbers - prints most except 2 and 4
- *
- * Return: void
+ * print_most_numbers - print numbers to 9 and exlclude 2, 4
  */
 void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
-		if ((i == 2) || (i == 4))
+		if (i != 50 && i != 52)
 		{
-			continue;
+			_putchar(i);
 		}
-		_putchar('0' + i);
 	}
 	_putchar('\n');
 }
