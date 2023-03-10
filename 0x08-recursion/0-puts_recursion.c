ea9596fe2ed65f7aea03c55be8042814ddf91ba9
Mfiso1 <you@example.com>
2023-03-09 18:57:06 -0800

    Task 0 done

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,14 +2,14 @@
 
 /**
 * _puts_recursion - Prints a string
-* @s: input
-* Return - Always 0 (Success)
+* @s: inputs string
 */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
 		_putchar(*s);
 		_puts_recursion(s + 1);
