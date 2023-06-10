72e7eba1bd67ce9fe887efb8d172a7ec4ab99148
Mfiso <mfsdmngomezulu@gmail.com>
2023-06-09 20:57:56 -0700

    ARGC

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints arguments
+ * @argc: number of commands
+ * @argv: array containing the program commands
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}i
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two integers
+ * @argc: number of commands
+ * @argv: array containing the program commands
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}i
