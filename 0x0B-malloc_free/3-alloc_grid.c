770e00d0e08a079e46162c58a22ae63576fd3ab4
Mfiso <mfsdmngomezulu@gmail.com>
2023-06-14 19:46:31 -0700

    Fixed another bug

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	twodimen = malloc(sizeof(int *) * height);
+	arry = malloc(sizeof(int *) * height);
 
 	if (arry == NULL)
 		return (NULL);
