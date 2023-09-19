221e1aaddf35b06cbbc875275a3464ab960ea458
Mfiso <mfsdmnomezulu@gmail.com>
2023-09-18 19:12:28 -0700

    new project

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character checked
+ * Return: 1 if uppercase or else 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
